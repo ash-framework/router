4fc7bd4f7de4da16d6b425e474109fe241627526
Richard Walker <digitalsadhu@gmail.com>
2016-10-26 22:09:08 +0200

    Add support for readable streams

--- src/route-success.js
+++ src/route-success.js
@@ -1,7 +1,12 @@
+const isReadableStream = require('./is-stream')
+
 module.exports = function (model, httpContext) {
   const {response} = httpContext
-  if (typeof model === 'object') {
+  if (isReadableStream(model)) {
+    model.pipe(response)
+  } else if (typeof model === 'object') {
     return response.json(model)
+  } else {
+    response.send(model)
   }
-  response.send(model)
 }

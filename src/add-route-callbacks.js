bacf09e38de33d85e43db800c95222f979834c2c
Richard Walker <digitalsadhu@gmail.com>
2016-11-11 10:01:03 +0100

    Clean up named routes

--- src/add-route-callbacks.js
+++ src/add-route-callbacks.js
@@ -39,77 +39,28 @@ function addRouteCallbacks (routeObjects, routesDir) {
       })
     } else {
       // explicit routes
-      let Route
-
-      if (fileExists(`${routeObj.name}.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.js`, routesDir)
+      const allowedMethods = ['get', 'put', 'post', 'patch', 'delete', 'options', 'head']
+      allowedMethods.forEach(method => {
+        let Route
+        if (method.toLowerCase() === 'get') {
+          if (fileExists(`${routeObj.name}.js`, routesDir)) {
+            Route = loadFile(`${routeObj.name}.js`, routesDir)
+          } else if (fileExists(`${routeObj.name}.get.js`, routesDir)) {
+            Route = loadFile(`${routeObj.name}.get.js`, routesDir)
+          }
+        }
+        if (!Route && fileExists(`${routeObj.name}.${method}.js`, routesDir)) {
+          Route = loadFile(`${routeObj.name}.${method}.js`, routesDir)
+        }
         if (Route) {
           objects.push({
-            method: 'get',
+            method: method,
             callback: createCallback(Route),
             name: routeObj.name,
             path: routeObj.path
           })
         }
-      } else if (fileExists(`${routeObj.name}.get.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.get.js`, routesDir)
-        if (Route) {
-          objects.push({
-            method: 'get',
-            callback: createCallback(Route),
-            name: routeObj.name,
-            path: routeObj.path
-          })
-        }
-      }
-
-      if (fileExists(`${routeObj.name}.post.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.post.js`, routesDir)
-        if (Route) {
-          objects.push({
-            method: 'post',
-            callback: createCallback(Route),
-            name: routeObj.name,
-            path: routeObj.path
-          })
-        }
-      }
-
-      if (fileExists(`${routeObj.name}.patch.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.patch.js`, routesDir)
-        if (Route) {
-          objects.push({
-            method: 'patch',
-            callback: createCallback(Route),
-            name: routeObj.name,
-            path: routeObj.path
-          })
-        }
-      }
-
-      if (fileExists(`${routeObj.name}.put.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.put.js`, routesDir)
-        if (Route) {
-          objects.push({
-            method: 'put',
-            callback: createCallback(Route),
-            name: routeObj.name,
-            path: routeObj.path
-          })
-        }
-      }
-
-      if (fileExists(`${routeObj.name}.delete.js`, routesDir)) {
-        Route = loadFile(`${routeObj.name}.delete.js`, routesDir)
-        if (Route) {
-          objects.push({
-            method: 'delete',
-            callback: createCallback(Route),
-            name: routeObj.name,
-            path: routeObj.path
-          })
-        }
-      }
+      })
     }
   })
   return objects

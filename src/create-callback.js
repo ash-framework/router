b4b397fa8dcea0171d2e07c866348103e243ca51
Richard Walker <digitalsadhu@gmail.com>
2016-11-11 16:29:04 +0100

    Delegate errors to express error handler

--- src/create-callback.js
+++ src/create-callback.js
@@ -1,18 +1,15 @@
 const HttpContext = require('@ash-framework/http-context')
 const routeChain = require('./route-chain')
-const routeError = require('./route-error')
 const routeSuccess = require('./route-success')
 
 module.exports = function (Route) {
-  return function (req, res) {
+  return function (req, res, next) {
     const httpContext = new HttpContext(req, res)
     const route = new Route(httpContext)
     routeChain(route)
       .then(model => {
         routeSuccess(model, httpContext)
       })
-      .catch(err => {
-        routeError(err, httpContext)
-      })
+      .catch(next)
   }
 }

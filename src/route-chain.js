c6f05c9e5cd0da6078424a32e7c9c758788a6f1d
Richard Walker <digitalsadhu@gmail.com>
2016-11-12 21:23:43 +0100

    Various tweaks to route chain

--- src/route-chain.js
+++ src/route-chain.js
@@ -1,29 +1,45 @@
 module.exports = function (route) {
   return Promise.resolve()
     .then(() => {
-      if (!route.deserialize) return
-      return route.deserialize()
+      if (route.deserialize) {
+        return route.deserialize()
+      }
     })
     .then(() => {
-      if (!route.beforeModel) return
-      return route.beforeModel()
+      if (route.beforeModel) {
+        return route.beforeModel()
+      }
     })
     .then(() => {
       return route.model()
     })
     .then(model => {
-      if (!route.afterModel) return model
-      return route.afterModel(model)
+      route.currentModel = model
+      const promises = [model]
+      if (route.afterModel) {
+        promises.push(route.afterModel(model))
+      }
+      return Promise.all(promises)
     })
     .then(model => {
-      if (!route.serialize) return model
-      return route.serialize(model)
+      const promises = [model[0]]
+      if (route.serialize) {
+        promises.push(route.serialize(model[0]))
+      }
+      return Promise.all(promises)
+    })
+    .then(model => {
+      return model[0]
     })
     .catch(err => {
-      let error
+      let error = err
       if (typeof route.error === 'function') {
-        error = route.error(err) || err
+        error = route.error(err)
       }
-      return Promise.reject(error)
+      if (error) {
+        return Promise.reject(error)
+      } else {
+        return route.currentModel
+      }
     })
 }
--- src/route-success.js
+++ src/route-success.js
@@ -6,7 +6,9 @@ module.exports = function (model, httpContext) {
     model.pipe(response)
   } else if (typeof model === 'object') {
     return response.json(model)
+  } else if (!model) {
+    response.send(String())
   } else {
-    response.send(model)
+    response.send(String(model))
   }
 }
